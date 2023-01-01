99e85c27c3faf186d75832d9f7800ec123f13f56
Andrius Makutas <andrius.makutas@gmail.com>
2023-01-01 22:26:36 +0200

    new years resolution challenge to complete as many as possible HR excersises

---
+++ NestedLists.py
@@ -0,0 +1,21 @@
+"""
+Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s)
+of any student(s) having the second lowest grade.Note: If there are multiple students with the second lowest grade,
+order their names alphabetically and print each name on a new line.
+Example:
+There are 5 students in this class whose names and grades are assembled to build the following list:
+python_students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+The lowest grade of 37.2 belongs to Tina. The second lowest grade of 37.21 belongs to both Harry and Berry,
+so we order their names alphabetically and print each name on a new line.
+"""
+
+
+def get_second_lowest_grades(students: list):
+    second_lowest_grades = []
+
+    return second_lowest_grades
+
+
+if __name__ == '__main__':
+    students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+    result = get_second_lowest_grades(students)
