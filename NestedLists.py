7ead8931a59bd87a4811f5ab05738bfe5b1955ba
Andrius Makutas <andrius.makutas@gmail.com>
2023-01-02 22:55:22 +0200

    solved nested lists excersise with students and scores finding the second lowest score

--- NestedLists.py
+++ NestedLists.py
@@ -9,13 +9,24 @@ The lowest grade of 37.2 belongs to Tina. The second lowest grade of 37.21 belon
 so we order their names alphabetically and print each name on a new line.
 """
 
-
-def get_second_lowest_grades(students: list):
-    second_lowest_grades = []
-
-    return second_lowest_grades
-
-
 if __name__ == '__main__':
-    students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
-    result = get_second_lowest_grades(students)
+    students_with_second_lowest_score = []
+    students_with_score = {}
+    for _ in range(int(input("Enter student number: "))):
+        name = input('Enter student name: ')
+        score = float(input('Enter student score as float: '))
+        students_with_score[name] = score
+    filter_out_identical_scores = set(students_with_score.values())
+
+    sorted_scores = sorted(filter_out_identical_scores)
+    second_lowest_score = sorted_scores[1]
+
+    for key, value in students_with_score.items():
+        if value == second_lowest_score:
+            students_with_second_lowest_score.append(key)
+
+    students_with_second_lowest_score = sorted(students_with_second_lowest_score)
+
+    for name in students_with_second_lowest_score:
+        print(name)
+
